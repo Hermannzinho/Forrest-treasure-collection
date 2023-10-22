9906a82f90d30be85f0d68d4d2d52ee0e79657d6
Hermannzinho <70027604+Hermannzinho@users.noreply.github.com>
2023-10-22 13:47:30 +0000

    Ability to move player

--- js/game.js
+++ js/game.js
@@ -3,3 +3,12 @@
 // Scene
 
 const gameScene = new Phaser.Scene("Game");
+
+// Initialization
+
+gameScene.init = function() {
+  this.playerSpeed = 1.5;
+  this.dragonSpeed = 2;
+  this.dragonMaxY = 280;
+  this.dragonMinY = 80;
+};
